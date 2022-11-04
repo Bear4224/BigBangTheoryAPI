2b1376499b6b79f2795329d0d70a7fe40694af9a
Jacob Showman <jacob.showman@mrjaffesclass.com>
2022-11-04 12:46:28 -0700

    End of Day 1 - Added code in tutorial, beginning debugging lesson

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,8 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
+const server = require('./server');
+const bbt361642 = functions.runWith({ memory: "2GB", timeoutSeconds: 60 })
+                     .https
+                     .onRequest(server);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+module.exports = { bbt361642 };
+
---
+++ functions/router.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const episodes = require('./routes/episodes');
+
+router.get("/episodes", episodes);
+
+module.exports = router;
