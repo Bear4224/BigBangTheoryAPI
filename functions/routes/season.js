47416521aba35e6e67a7450ad371d6295f5b9f52
Jacob Showman <jacob.showman@mrjaffesclass.com>
2022-11-14 12:45:20 -0800

    Finished Route 1 - Diplay all episodes in a season

--- functions/routes/season.js
+++ functions/routes/season.js
@@ -1,6 +1,10 @@
 const bbt = require('big-bang-theory');
 
 const season = (req, res) => {
+    function seasonSort(episode){
+    
+        return episode.season == req.params.seasonNum;
+    }
   if (req.params.seasonNum > 12){
     res.status(200).json({
       success: false, 
@@ -12,14 +16,12 @@ const season = (req, res) => {
     res.status(200).json({
       success: true,
       seasonNum: req.params.seasonNum,
-      data: bbt._embedded.episodes.filter(seasonSort, req.params.seasonNum),
+      data: bbt._embedded.episodes.filter(seasonSort),
     });
   }
   
 }
 
-function seasonSort(episode, seasonNum){
-    return episode.season == seasonNum;
-}
+
 
 module.exports = season;
